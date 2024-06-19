42d90016a8deba3d48f47b7c6cda7eff247e300e
ValerioL96 <v.lembo1396@gmail.com>
2024-06-19 19:08:52 +0200

    Aggiunto stile slides

--- src/components/MainCardsSlider.vue
+++ src/components/MainCardsSlider.vue
@@ -26,18 +26,18 @@ export default{
                 image:'src/assets/img/training-box-4.jpg',
                 view: true,
             },
-            {
-                name:'Pedaling',
-                image:'src/assets/img/training-box-5.jpg',
-                view: true,
-            },
+            //{
+                //name:'Pedaling',
+                //image:'src/assets/img/training-box-5.jpg',
+                //view: true,
+            //},
         ]
      
         }
     },
 
     methods:{
-        prevSlide: function(){
+        prevSlide(){
             if(this.activeIndex===0){
                 this.activeIndex=this.slidesObject.length-1;
             }else{
@@ -45,7 +45,7 @@ export default{
             }
         },
 
-        nextSlide: function(){
+        nextSlide(){
             if(this.activeIndex===this.slidesObject.length-1){
                 this.activeIndex=0;
             }else{
@@ -60,26 +60,31 @@ export default{
 
 <template>
     <section>
-        <h2>Our Professional Cycling Trainings</h2>
-        <p>Learn from the mountain biking expert.</p>
+        <div class="title-slides">
+            <h2>Our Professional Cycling Trainings</h2>
+            <p>Learn from the mountain biking expert.</p>
+        </div>
+      
         <div class="container">
-            <div class="slider-wrapper" tabindex="0">
-                <div class="item" v-for="(slide,index) in slidesObject" :key="index">
-                    <img :src="slide.image" :alt="slide.name"/>
+            <div class="slider-wrapper">
+                
+                <div class="item" v-for="slide in slidesObject">
                     <div class="text">
                         <h3>{{ slide.name}}</h3>
                         <p>
                            {{slide.view }}
                         </p>
                     </div>
+                    <img :src="slide.image" :alt="slide.name"/>
+                   
                 </div>
 
                 
             </div>
 
-            <div class="thumbs">
-                    <button class="prev" @click="prevSlide()"><i class="fa-solid fa-arrow-right"></i></button>
-                    <button class="next" @click="nextSlide()"><i class="fa-solid fa-arrow-right"></i></button>
+            <div class="thumbs" >
+                    <button class="prev" @click="prevSlide()"><i class="fa-solid fa-chevron-left"></i></button>
+                    <button class="next" @click="nextSlide()"><i class="fa-solid fa-chevron-right"></i></button>
             </div>
         </div>
     </section>
@@ -89,15 +94,45 @@ export default{
 
 <style lang="scss" scoped>
 
+.title-slides{
+    text-align: center;
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+}
+
 .slider-wrapper{
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+
+    .item{
+        border: solid 0.5rem black;
+
+    }
+
+    .text{
+        position:relative;
+        top: 50px;
+        left: 10px;
+        align-content: space-between;
+        color:white;
+        z-index: 1;
+        
+    }
     
 }
 
 .thumbs{
-background-color: greenyellow;
+text-align: center;
+padding: 2rem 0rem;
+
+button{
+    background-color: white;
+    margin-left: 1rem;
+    height: 80px;
+    width: 80px;
+}
+
 }
 
 </style>
