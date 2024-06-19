62196f5debbc323338b6e6a34efd039b33083f34
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-19 11:48:40 +0200

    Base scaffolding

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+
+import AppMain from '../src/components/AppMain.vue';
+import AppHeader from '../src/components/AppHeader.vue';
+import AppFooter from '../src/components/AppFooter.vue';
+
+export default{
+    components:{
+      AppMain,
+      AppHeader,
+      AppFooter
+    },
+      data() {
+          return{
+
+          }
+      }
+}
+
+</script>
+
+<template>
+
+<AppHeader/>
+<AppMain/>
+<AppFooter/>
+
+</template>
+
+<style lang="scss">
+
+@use 'components/styles/general.scss';
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+
+import {store} from '../store.js';
+
+export default{
+    data() {
+        return{
+            store,
+        }
+    }
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
