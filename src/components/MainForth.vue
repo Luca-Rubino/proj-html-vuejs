fc489f1eb1c442ef921e561417e172dcfca4cee1
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-20 13:02:53 +0200

    sistemazione array e img

--- src/components/MainForth.vue
+++ src/components/MainForth.vue
@@ -6,9 +6,9 @@
         </div>
         <div class="trainers-container">
             <div class="trainer-card" v-for="(trainer, index) in trainers" :key="index">
-                <img src="../assets/img/biker1.jpg">
-                <h3>{{ trainers.name }}</h3>
-                <p>{{ trainers.role }}</p>
+                <img :src="trainer.image" :alt="trainer.name">
+                <h3>{{ trainer.name }}</h3>
+                <p>{{ trainer.role }}</p>
                 <div class="social-icons">
                     <i class="fa-brands fa-facebook-f"></i>
                     <i class="fa-brands fa-square-instagram"></i>
@@ -20,27 +20,32 @@
 </template>
 
 <script>
+import Biker1 from '../assets/img/biker1.jpg';
+import Biker2 from '../assets/img/biker2.jpg';
+import Biker3 from '../assets/img/biker3.jpg';
+import Biker4 from '../assets/img/biker4.jpg';
+
 export default {
     data() {
         return {
             trainers: [
                 {
-                    image: require('../assets/img/biker1.jpg'),
+                    image: Biker1,
                     name: 'Michael Lee',
                     role: 'Biker',
                 },
                 {
-                    image: require('../assets/img/biker2.jpg'),
+                    image: Biker4,
                     name: 'Robert Lee',
                     role: 'Biker',
                 },
                 {
-                    image: require('../assets/img/biker3.jpg'),
+                    image: Biker3,
                     name: 'John Doe',
                     role: 'Biker',
                 },
                 {
-                    image: require('../assets/img/biker4.jpg'),
+                    image: Biker2,
                     name: 'Emily Brown',
                     role: 'Biker',
                 },
@@ -48,15 +53,17 @@ export default {
         };
     },
 };
+
 </script>
 
 <style scoped lang="scss">
 .trainers-section {
     text-align: center;
-    margin: 2rem 0;
+    margin-bottom: 8rem;
+    background-color: #fbfbfb;
 }
 
-.margin-bottom{
+.margin-bottom {
     margin-bottom: 3rem;
 }
 
@@ -71,10 +78,10 @@ export default {
 
 .trainer-card {
     background: #fff;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     text-align: center;
-    width: calc(100%/5);
     background-color: black;
+    border: solid .8rem #f7f7f7;
+    width: calc(100% / 4 - 2rem);
 }
 
 .trainer-card img {
@@ -85,19 +92,24 @@ export default {
 .trainer-card h3 {
     font-size: 1.2rem;
     color: white;
+    font-weight: bolder;
 }
 
 .trainer-card p {
     margin: 0.5rem 0;
     font-size: 0.9rem;
     color: white;
+    color: #b3b3b3;
+    font-size: .8rem;
+    font-weight: bolder;
 }
 
 .social-icons i {
+    font-size:.8rem;
     margin: 0 0.5rem;
-    color: #555;
+    color: white;
     cursor: pointer;
     color: white;
-    margin-bottom:1.5rem
+    margin-bottom: 1.5rem
 }
 </style>
