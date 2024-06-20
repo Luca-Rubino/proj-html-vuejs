fae4d45d2c62512a3c4ec68d591c9bb37e4b5ed9
ValerioL96 <v.lembo1396@gmail.com>
2024-06-20 12:48:11 +0200

    Aggiunto pacchi promozionali

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,13 +2,15 @@
 import {store} from '../store.js';
 import MainSixthArticle from './MainSixthArticle.vue'; 
 import MainFirstGear from './MainFirstGear.vue'
-import MainThirdCard from './MainThirdCard.vue'
+import MainThirdCard from './MainThirdCard.vue';
+import MainSpecialPacks from './MainSpecialPacks.vue';
 
 export default{
     components:{
         MainFirstGear,
         MainThirdCard,
         MainSixthArticle,
+        MainSpecialPacks,
     },
     data() {
         return{
@@ -24,6 +26,7 @@ export default{
         <MainFirstGear/>
         <MainThirdCard/>
         <MainSixthArticle/>
+        <MainSpecialPacks/>
     </main>
 </template>
 
