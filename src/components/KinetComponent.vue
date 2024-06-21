98c2ec7fce1d8214f8aea61853fd532e5c0e5f44
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-21 17:11:18 +0200

    installato kinet e aggiunta animazione per il curosore

---
+++ src/components/KinetComponent.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-inner">
+      <h1>github.com/gmrchk/kinet</h1>
+    </div>
+    <div id="circle" class="circle"></div>
+  </div>
+</template>
+
+<script>
+import Kinet from 'kinet';
+
+export default {
+  name: 'KinetComponent',
+  data() {
+    return {
+      scrollX: 0,
+      scrollY: 0,
+      mouseX: 0,
+      mouseY: 0
+    };
+  },
+  mounted() {
+    // create instance of kinet with custom settings
+    this.kinet = new Kinet({
+      acceleration: 0.06,
+      friction: 0.20,
+      names: ["x", "y"],
+    });
+
+    // select circle element
+    const circle = this.$el.querySelector('#circle');
+
+    // set handler on kinet tick event
+    this.kinet.on('tick', (instances) => {
+      const translateX = instances.x.current;
+      const translateY = instances.y.current;
+      circle.style.transform = `translate3d(${translateX}px, ${translateY}px, 0) rotateX(${instances.x.velocity / 2}deg) rotateY(${instances.y.velocity / 2}deg)`;
+    });
+
+    // call kinet animate method on mousemove
+    document.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('scroll', this.handleScroll);
+
+    // log events
+    this.kinet.on('start', function() {
+      console.log('start');
+    });
+
+    this.kinet.on('end', function() {
+      console.log('end');
+    });
+  },
+  beforeDestroy() {
+    // Clean up event listeners and kinet instance
+    document.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('scroll', this.handleScroll);
+    this.kinet.off('tick');
+    this.kinet.off('start');
+    this.kinet.off('end');
+  },
+  methods: {
+    handleMouseMove(event) {
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
+      this.kinet.animate('x', this.mouseX - window.innerWidth / 2 + this.scrollX);
+      this.kinet.animate('y', this.mouseY - window.innerHeight / 2 + this.scrollY);
+    },
+    handleScroll() {
+      this.scrollX = window.scrollX;
+      this.scrollY = window.scrollY;
+      this.kinet.animate('x', this.mouseX - window.innerWidth / 2 + this.scrollX);
+      this.kinet.animate('y', this.mouseY - window.innerHeight / 2 + this.scrollY);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  cursor: default;
+}
+
+h1 {
+  font-size: 50px;
+  margin: 0 auto 10px;
+}
+
+a {
+  color: #232323;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.circle {
+  width: 60px;
+  height: 60px;
+  background: transparent;
+  border: solid rgba(197, 196, 196, 0.747) 2px;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: 26rem -1.1rem ;
+  transform: translate3d(0, 0, 0);
+  pointer-events: none;
+  z-index: 10;
+}
+</style>
